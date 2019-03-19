52d446eae0a35996128366bb58c7e1cb69191e5d
ch264 <christinavhastenrath@gmail.com>
2019-03-18 19:32:50 -0700

    added profile page and sub recipe page

--- models.py
+++ models.py
@@ -1,6 +1,17 @@
 from app import db, marshmallow
 from flask import jsonify
 
+# inmport gravatar 
+from hashlib import md5
+
+# add this to model user for the gravatar
+class User(UserMixin, db.Model):
+    # ...
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
+
 
 
 if __name__ == 'models':
