da2f3a4b54cc41e45f4370274e32a65f5a4355f2
brandonmcastillo <brandonmaerina@gmail.com>
2019-03-19 00:54:11 -0700

    Updated models

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 from flask import Flask, g
 from flask import Flask, request
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, session, escape
+import models
 
 DEBUG = True
 PORT = 8000
@@ -21,32 +22,33 @@ def after_request(response):
   g.db.close()
   return response
 
-
-
-
-
-
 @app.route('/')
 def index():
 
 @app.route('/about')
 def about():
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+ 
 
 @app.route('/signup', methods=['POST'])
 def signup():
 
 @app.route('/logout')
-def loguout():
+def logout():
 
-@app.route('/profile', methods=['GET', 'PUT', 'POST'])
-def profile():
+
+@app.route('/profile', methods=['GET', 'PUT'])
+@app.route('/profile/<id>', methods=['GET'])
+
 
 @app.route('/recipes', methods=['GET', 'POST'])
 @app.route('/recipes/<id>', methods=['GET', 'PUT', 'POST', 'DELETE'])
-def get_all_recipes():
+
+
+
+
 
 
 
